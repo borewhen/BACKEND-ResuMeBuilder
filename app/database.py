71f7eed2ce3a5c7d5d24e48ab866f59f4d3bd07e
bryan <bryan.rustardy@kabam.ai>
2025-02-05 16:06:16 +0800

    change db setup

--- app/database.py
+++ app/database.py
@@ -8,7 +8,11 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgrespw@host.docker.internal/resume_db")
 
 print(DATABASE_URL)
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=5,
+    max_overflow=10
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 

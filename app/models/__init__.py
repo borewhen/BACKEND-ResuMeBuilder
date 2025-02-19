ab8764b2df151227333ef70777dd72660ed530bd
bryan <bryan.rustardy@kabam.ai>
2025-02-19 23:47:43 +0800

    fix imports on models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,6 @@ from app.models.experience import Experience
 from app.models.job import Job
 from app.models.job_application import JobApplication
 from app.models.user import User
+from app.models.skills import Skill
+from app.models.job_application import JobApplication
+from app.models.interview_questions import InterviewQuestion

7b7d3e661feafae8717b91f65b3acda67233321e
bryan <bryan.rustardy@kabam.ai>
2025-03-05 17:34:32 +0800

    fix

--- app/routers/mock_interview.py
+++ app/routers/mock_interview.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Query, Path
+import os
 from app.models import User
 from app.service.user_service import jwt_required
 from app.service.job_service import scrape_jobs_list, scrape_job_detail
@@ -8,6 +9,8 @@ from fastapi.exceptions import ResponseValidationError
 
 router = APIRouter()
 
-@router("/")
-def generate_interview_topics():
+OPEN_AI_API_KEY = os.getenv("OPEN_API_KEY", "")
+
+@router.get("/{job_id}")
+def generate_interview_topics(job_id: Annotated[int, Path(title="The ID of the item to get")]):
     pass

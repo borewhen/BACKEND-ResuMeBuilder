e658216d21e6d670e2dd0457d3dcf8e4fea59aa9
bryan <bryan.rustardy@kabam.ai>
2025-03-05 20:07:14 +0800

    add job desc formatting

--- app/routers/mock_interview.py
+++ app/routers/mock_interview.py
@@ -3,7 +3,7 @@ import os
 from app.models import User
 from app.service.user_service import jwt_required
 from sqlalchemy.orm import Session
-from app.service.job_service import scrape_job_detail
+from app.service.job_service import scrape_job_detail, get_job_skills_required, parse_skills_from_job_desc
 from app.database import get_db
 from typing import Annotated
 
@@ -18,7 +18,11 @@ def generate_interview_topics(job_id: Annotated[int, Path(title="The ID of the i
         job_description = job_detail.job_description
 
         # check if this job has been put in DB or not
+        skills = get_job_skills_required(job_id)
+        if not skills:
+            # parse skills from description using OpenAI
+            skills = parse_skills_from_job_desc(job_detail.job_description)
 
-
+        return "success", 200
     except Exception:
         raise HTTPException(status_code=500)
--- app/service/job_service.py
+++ app/service/job_service.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI, HTTPException
 import requests
 import os
+from app.models.skills import Skill
+import openai
+import re
 
 LINKEDIN_SCRAPER_API_KEY=os.getenv("LINKEDIN_SCRAPER_API_KEY", "dummy_key")
 
+
 def scrape_jobs_list(field: str, page: str):
+    """
+    gets job list from scraping linkedin
+    field (str): company / job name
+    """
     response = requests.get(
         f"https://api.scrapingdog.com/linkedinjobs",
         params={"api_key": LINKEDIN_SCRAPER_API_KEY, "field": field, "page": page, "geoid": 102454443},
@@ -14,11 +22,61 @@ def scrape_jobs_list(field: str, page: str):
         raise HTTPException(status_code=500, detail=f"Error fetching jobs: {response.text}")
     return response.json()
 
+
 def scrape_job_detail(job_id):
-    response = response = requests.get(
-        f"https://api.scrapingdog.com/linkedinjobs",
-        params={"api_key": LINKEDIN_SCRAPER_API_KEY, "job_id": {job_id}},
-    )
-    if response.status_code != 200:
-        raise HTTPException(status_code=500, detail=f"Error fetching jobs: {response.text}")
-    return response.json()
+    """
+    gets job detail from scraping linkedin
+    job_id (int): this is unique identifier for job
+    """
+    try:
+        response = response = requests.get(
+            f"https://api.scrapingdog.com/linkedinjobs",
+            params={"api_key": LINKEDIN_SCRAPER_API_KEY, "job_id": {job_id}},
+        )
+        if response.status_code != 200:
+            raise HTTPException(status_code=500, detail=f"Error fetching jobs: {response.text}")
+        
+        job_detail = response.json()
+        completion = openai.ChatCompletion.create(
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"""
+                    Format the following job description as a valid HTML string that can be directly rendered on a webpage:
+                    
+                    ---
+                    {job_detail[0].get("job_description", "No description available.")}
+                    ---
+                    
+                    Ensure it includes proper HTML tags (such as <h1>, <p>, <ul>, <li> where needed), and format them under one <div> with class name of job-desc-container. Don't include escaped newlines (\n) and backslashes (\\)
+                    """
+                }
+            ]
+        )
+
+        raw_html = completion["choices"][0]["message"]["content"].strip()
+        formatted_html = re.sub(r"^```html\n|```$", "", raw_html).strip()
+        job_detail[0]["job_description"] = "".join(line.strip() for line in formatted_html.replace("\\n", "").replace('\\"', '"').split("\n"))
+        return job_detail
+    except Exception as e:
+        raise HTTPException(status_code=500)
+
+
+def get_job_skills_required(db, job_id):
+    """
+    gets assosciated skills from the job
+    job_id (int): this is unique identifier for job
+    """
+    skills = db.query(Skill).filter(job_id == job_id).all()
+    if not skills: 
+        return None
+    return skills.__repr__()
+
+
+def parse_skills_from_job_desc(db, job_description):
+    """
+    parse job description and post this to DB
+    job_description (str)
+    """
+    pass

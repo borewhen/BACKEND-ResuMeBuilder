c394129a9ed73aff109161f6446438d5f77386f8
Bowen <bruceshen0323@gmail.com>
2025-04-18 06:25:26 +0800

    CORS AGAIN

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origin_regex=r"^https:\/\/(resume|uhired)-.*-borewhens-projects\.vercel\.app$",
+    allow_origin_regex=r"^https:\/\/(resume|uhired)-.*\.vercel\.app$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
